9fa1f6e54708015b50fdbc6291ce5276f3d6886a
unnath123 <unnathpgowda@gmail.com>
2023-09-23 01:58:54 +0530

    buttons functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const tRow = document.getElementById("TH-Row");
 const tBody = document.getElementById("t-Body")
 
+const bold_btn = document.getElementById("btn-bold")
+const btn_italic= document.getElementById("btn-italic")
+const underline_btn = document.getElementById("btn-underline")
+
+
 function createCol(type,WhichRow,rowNum){
     for(let col=0;col<26;col++){
         let cell = document.createElement(type);
@@ -18,7 +23,6 @@ function createCol(type,WhichRow,rowNum){
         WhichRow.append(cell)
     }
 }
-
 
 function SetHeadColor(element,colr){
     const col = document.getElementById(`${element.id[0]}`);
@@ -69,11 +73,54 @@ function CreateRow(){
         // }
        
         tBody.append(trow)
-    
     }
 }
 
+function btnActivity(whatStyle, prop, whichBtn){
+    if(CurrentCell.style[whatStyle] == prop){
+        if(prop == 'underline'){
+            CurrentCell.style[whatStyle] = 'none' 
+        }
+        else{
+            CurrentCell.style[whatStyle] = 'normal'
+        }
+                
+                whichBtn.style.backgroundColor = "transparent";
+            }
+            else{
+                CurrentCell.style[whatStyle] = prop;
+                whichBtn.style.backgroundColor = "#C0C0C0";
+            }
+}
+bold_btn.addEventListener("click",()=>btnActivity("fontWeight", "bold", bold_btn))
+//   btnActivity()
 
+// bold_btn.addEventListener("click",()=>{
+//     if(CurrentCell.style.fontWeight == 'bold'){
+//         CurrentCell.style.fontWeight = 'normal'
+//         bold_btn.style.backgroundColor = "transparent";
+//     }
+//     else{
+//         CurrentCell.style.fontWeight = 'bold';
+//         bold_btn.style.backgroundColor = "#C0C0C0";
+//     }
+// })
+
+
+btn_italic.addEventListener("click",()=>btnActivity("fontStyle", "italic", btn_italic ))
+// btn_italic.addEventListener("click",()=>{
+//     if(CurrentCell.style.fontStyle == 'italic'){
+//         CurrentCell.style.fontStyle = 'normal'
+//         btn_italic.style.backgroundColor = "transparent";
+//     }
+//     else{
+//         CurrentCell.style.fontStyle = 'italic';
+//         btn_italic.style.backgroundColor = "#C0C0C0";
+//     }
+// })
+
+underline_btn.addEventListener("click",()=>btnActivity("textDecoration", "underline", underline_btn))
 
 createCol("th",tRow)
 CreateRow()
+
